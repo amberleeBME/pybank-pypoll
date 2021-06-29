97dfe24818b4ab0c5dc17320702b28a2cba2c7f6
Amber <amberleeBME@outlook.com>
2021-06-28 22:22:29 -0400

    update PyPoll main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,9 +9,32 @@
     # 3. Export a text file with the results
 # ---------------------------------------------------------
 
-#Import os and csv module 
+# Variables
+votersCount = 0
+
+# Import os and csv module 
 import os
 import csv
 
 # Access "../Resources/election_data.csv"
 csvpath = os.path.join('Resources', 'election_data.csv')
+
+# Read CSV file
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        votersCount = votersCount + 1
+
+line1 = f"Total Votes: {votersCount}"
+print(line1)
+
+# Write analysis to text file
+# with open("PyPoll.txt", "w") as file:
+#     file.writelines(analysis)
