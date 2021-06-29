2f67eeb9b25704230a5b7b607cc469641372fe37
Amber <amberleeBME@outlook.com>
2021-06-28 21:16:13 -0400

    Update PyBank main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,14 +10,20 @@
  # 3. Export a text file with the results
 #-----------------------------------------------------------------------------
 
-#Import os module 
-import os
+# Define variables
+months = 0
+net = 0
+delta = []
+avg = 0
+inc = 0
+dec = 0
 
-# Import csv module 
+#Import os and csv module 
+import os
 import csv
 
 # Access "../Resources/budget_data.csv"
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # Read CSV file
 with open(csvpath) as csvfile:
@@ -25,12 +31,39 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+
+        #Count the number of months
+        months = months + 1 
+
+        #Calculate net total
+        net = net + (int(row[1]))
+
+        #Calculate monthly change and append to list
+        if months > 1:
+            delta.append(int(row[1])-prev)
+
+        # Store current month's profit/loss
+        prev = int(row[1])
+
+# Calculate average monthly change
+avg = round(sum(delta)/len(delta),2)
+
+# Calculate max monthly change
+inc = max(delta)
+
+# Calculate min monthly change
+dec = min(delta)
+
+
+
+# Print analysis to terminal
+print(f"Total Number of months: {months}")
+print(f"Net Total: ${net}")
+print(f"Average change: ${avg}")
+print(f"Greatest increase: ${inc}")
+print(f"Greatest decrease: ${dec}")
